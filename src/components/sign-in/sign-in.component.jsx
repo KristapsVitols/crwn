633222003ab6ba8fdbc42e9b813153363d4f7e4a
kristapsv <kristapsvitols91@gmail.com>
2019-09-10 22:13:31 +0300

    redux-saga refactor

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {auth, SignInWithGoogle} from '../../firebase/firebase.utils';
+import {googleSignInStart, emailSignInStart} from '../../redux/user/user.actions';
 
 import './sign-in.styles.scss';
 
@@ -18,17 +20,10 @@ class SignIn extends React.Component {
 
     handleSubmit = async event => {
         event.preventDefault();
-
+        const {emailSignInStart} = this.props;
         const {email, password} = this.state;
 
-        try {
-            await auth.signInWithEmailAndPassword(email, password);
-            this.setState({email: '', password: ''});
-        } catch (err) {
-            console.log(err);
-        }
-
-        this.setState({email: '', password: ''});
+        emailSignInStart(email, password);
     };
 
     handleChange = event => {
@@ -38,6 +33,7 @@ class SignIn extends React.Component {
     };
 
     render() {
+        const {googleSignInStart} = this.props;
         return (
             <div className='sign-in'>
                 <h2>I already have an account</h2>
@@ -52,10 +48,8 @@ class SignIn extends React.Component {
                                handleChange={this.handleChange}/>
 
                     <div className='buttons'>
-                        <CustomButton type="submit">
-                            Sign in
-                        </CustomButton>
-                        <CustomButton onClick={SignInWithGoogle} isGoogleSignIn>
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn>
                             Sign in With Google
                         </CustomButton>
                     </div>
@@ -65,4 +59,9 @@ class SignIn extends React.Component {
     }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => dispatch(emailSignInStart({email, password})),
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {auth, createUserProfileDocument} from '../../firebase/firebase.utils';
+import {signUpStart} from '../../redux/user/user.actions';
 
 import './sign-up.styles.scss';
+import {signUp} from '../../redux/user/user.saga';
 
 class SignUp extends React.Component {
     constructor() {
@@ -21,25 +23,15 @@ class SignUp extends React.Component {
 
     handleSubmit = async event => {
         event.preventDefault();
+        const {signUpStart} = this.props;
         const {displayName, email, password, confirmPassword} = this.state;
 
         if (password !== confirmPassword) {
-            alert('Passwords don\'t match!');
+            alert("Passwords don't match!");
             return;
         }
 
-        try {
-            const {user} = await auth.createUserWithEmailAndPassword(email, password);
-            await createUserProfileDocument(user, {displayName});
-            this.setState({
-                displayName: '',
-                email: '',
-                password: '',
-                confirmPassword: '',
-            });
-        } catch (err) {
-            console.error(err);
-        }
+        signUpStart({displayName, email, password});
     };
 
     handleChange = event => {
@@ -94,4 +86,8 @@ class SignUp extends React.Component {
     }
 }
 
-export default SignUp;
+const mapDispatchToProps = dispatch => ({
+    signUpStart: userCredentials => dispatch(signUpStart(userCredentials)),
+});
+
+export default connect(null, mapDispatchToProps)(SignUp);
